656efe891a7a2287846535fd68bea410b37b091a
DisistiNao <gabrielsaldanha373@gmail.com>
2021-08-23 12:28:48 -0300

    first commit

---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.css"
+import React from "react"
+
+const Home = props => (
+    <aside className="Home">
+        <h1>Inicio</h1>
+        <p>Bem Vindo!</p>
+    </aside>
+)
+
+export default Home
